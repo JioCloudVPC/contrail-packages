46935fb20c2f500641b5fe957e5b14f1a1b4672f
Soren Hansen <Soren.Hansen@ril.com>
2014-06-02 21:04:44 +0200

    Add a monotonically increasing component to version string
    This allows for upgrades to happen approproately if packages are
    published in an APT repository.
    
    (cherry picked from commit 75d805020a722b02e2cba159a860975ec94cab54)
    
    Conflicts:
    	versions.mk

--- versions.mk
+++ versions.mk
@@ -15,11 +15,14 @@ else
 # TODO: store the git refs for all projects in the manifest in a file
 #
 
+CONTROLLER_COMMIT_NUMBER := $(shell cd controller; git rev-list --abbrev-commit HEAD | wc -l)
 CONTROLLER_REF := $(shell (cd controller; git log --oneline -1) | awk '/[0-9a-f]+/ { print $$1; }')
+NEUTRON_COMMIT_NUMBER := $(shell cd openstack/neutron_plugin; git rev-list --abbrev-commit HEAD | wc -l)
 NEUTRON_REF := $(shell (cd openstack/neutron_plugin; git log --oneline -1) | awk '/[0-9a-f]+/ { print $$1; }')
+WEBUI_COMMIT_NUMBER := $(shell cd contrail-web-core; git rev-list --abbrev-commit HEAD | wc -l)
 WEBUI_REF := $(shell (cd contrail-web-core; git log --oneline -1) | awk '/[0-9a-f]+/ { print $$1; }')
-CONTRAIL_VERSION = 1.1master~$(CONTROLLER_REF)
-NEUTRON_VERSION = 1.1master~$(NEUTRON_REF)
-WEBUI_VERSION = 1.1master~$(WEBUI_REF)
+CONTRAIL_VERSION = 1.1master~$(CONTROLLER_COMMIT_NUMBER)+$(CONTROLLER_REF)
+NEUTRON_VERSION = 1.1master~$(NEUTRON_COMMIT_NUMBER)+$(NEUTRON_REF)
+WEBUI_VERSION = 1.1master~$(WEBUI_COMMIT_NUMBER)+$(WEBUI_REF)
 
 endif
